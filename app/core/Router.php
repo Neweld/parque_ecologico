15b7338e11c971c2d93bf98b660c5620845bf794
Gabriel <bielzaca@gmail.com>
2026-05-02 00:16:36 -0300

    correçao urls router

--- app/core/Router.php
+++ app/core/Router.php
@@ -50,9 +50,9 @@ class Router {
 
         //rotas de admin
         $router->get('/api/agendamentos/listar', 'AgendamentoController@index', 'AuthMiddleware');
-        $router->put('api//agendamentos/aprovar/{id}', 'AgendamentoController@aprovar', 'AuthMiddleware');
-        $router->put('api/agendamentos/rejeitar/{id}', 'AgendamentoController@rejeitar', 'AuthMiddleware');
-        $router->delete('api/agendamentos/excluir/{id}', 'AgendamentoController@delete', 'AuthMiddleware');
+        $router->put('/api//agendamentos/aprovar/{id}', 'AgendamentoController@aprovar', 'AuthMiddleware');
+        $router->put('/api/agendamentos/rejeitar/{id}', 'AgendamentoController@rejeitar', 'AuthMiddleware');
+        $router->delete('/api/agendamentos/excluir/{id}', 'AgendamentoController@delete', 'AuthMiddleware');
 
 
         
